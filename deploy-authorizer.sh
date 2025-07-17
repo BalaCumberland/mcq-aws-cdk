798915c09cb0111aaf5aaba848842f278815fba9
BalaCumberland <37269795+BalaCumberland@users.noreply.github.com>
2025-07-16 21:41:19 -0700

    scripts for deployment and db conn

---
+++ deploy-authorizer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd authorizer-lambda
+mkdir -p deployment
+cd deployment
+cp -r ../index.js ../package.json ../package-lock.json ../node_modules ./
+zip -r ../function.zip ./*
+cd ..
+aws lambda update-function-code --function-name firebase-authorizer --zip-file fileb://function.zip
+rm -rf deployment
+echo "Authorizer deployed successfully!"
